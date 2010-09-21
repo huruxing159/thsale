204f8a27db71d3f8f9762d6fc1a7da6a2a843cc5
huruxingwindow <huruxing159@sina.com>
2010-09-21 14:34:34 +0800

    add timer

--- src/main/java/com/goodhope/thsale/factory/SpringBeanFactory.java
+++ src/main/java/com/goodhope/thsale/factory/SpringBeanFactory.java
@@ -6,8 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringBeanFactory {
 
 	private static ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "classpath:applicationContext.xml", "classpath:applicationContext-mail.xml", "classpath:applicationContext-util.xml", "classpath:applicationContext-service.xml",
-	// "classpath:applicationContext-timer.xml"
-			});
+			"classpath:applicationContext-timer.xml" });
 
 	public static Object getBean(String alias) {
 		return context.getBean(alias);
